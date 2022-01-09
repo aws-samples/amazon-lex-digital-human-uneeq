8d69872c9cc421b0a04f19a1bb33550bfc12cb15
Barry Conway <conwbarr@amazon.com>
2022-01-10 10:36:19 +1100

    updating the comments

--- function/index.js
+++ function/index.js
@@ -4,9 +4,10 @@
  * [Digital Human]<--->[API Gateway]<--->[this AWS Lambda function]<--->[Amazon Lex]
  *
  * The AWS Lambda function must have the following environment variables configured:
- * 1. LEXBOT_NAME - the name of the Amazon Lex bot
- * 2. LEXBOT_ALIAS - the name of the Amazon Lex bot alias
+ * 1. LEXBOT_ID - the Lex bot ID
+ * 2. LEXBOT_ALIAS_ID - the Lex bot alias ID
  * 3. WELCOME_INTENT - the name of the welcome intent configured as aprt of the Lex bot
+ * 4. LEXBOT_LOCALE_ID - the language and locale for the lex bot
  
  ******************/
  const uuid = require('uuid')
@@ -49,13 +50,8 @@ exports.lambda_handler = async function(event) {
     let lexruntimev2 = new AWS.LexRuntimeV2()
 
  switch (fmType) {
-     /* The 'type' field in 'fm-avatar' specifies if this is the beginning of a new conversation
-      * with a persona ('WELCOME'), or the next request in a continuing conversation ('QUESTION') */
      case 'WELCOME':
          console.debug('Type is WELCOME')
-         /* Lex uses the userId field in the query params to maintain session state during a
-          * conversation - the value is user-defined, and then passed in the conversationPayload
-          * so that it is returned in the next request */
          params.sessionId = uuid.v4(),
          params.sessionState.intent = {
                 name: process.env.WELCOME_INTENT,
@@ -76,9 +72,6 @@ exports.lambda_handler = async function(event) {
          console.debug('Type is QUESTION')
          conversationPayload = JSON.parse(fmConversation)
          params.sessionId = conversationPayload.platformSessionId
-         /* Lex returns an error if an empty string is passed as the input text - replacing
-          * the empty string with a period triggers the fallback intent, so the end user hears a
-          * fallback/clarification response */
           if (fmQuestion == '') {
             params.text = '.'
           } else {
@@ -97,9 +90,6 @@ exports.lambda_handler = async function(event) {
     }
 
 /**
- * Parses the message field from the response to the putSession or postText methods, to extract the dialog
- * that will be spoken by the digital human, and any instruction payloads that have been defined as
- * parameters in the matched intent, and returns an answer string and instructions JSON for the response payload.
  *
  * @param {object} response The response JSON from Lex
  * @return {string} answer and instructions (stringified JSON)
